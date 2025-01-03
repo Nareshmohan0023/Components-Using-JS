d291ad229255401d991a707873bc14bddb4069de
spidey0023 <nareshmohan0023@gmail.com>
2025-01-03 23:42:32 +0530

    Component added

--- Slider_Component/script.js
+++ Slider_Component/script.js
@@ -14,20 +14,58 @@ function Move_Slide(cur_slide) {
 }
 
 RB.addEventListener("click", () => {
-  if (cur_slide === slides.length - 1) {
+  if (cur_slide >= slides.length - 1) {
     cur_slide = 0;
     Move_Slide(cur_slide);
+    dotChanger();
   } else {
     cur_slide++;
     Move_Slide(cur_slide);
+    dotChanger();
   }
 });
 LB.addEventListener("click", () => {
   if (cur_slide === 0) {
     cur_slide = slides.length - 1;
     Move_Slide(cur_slide);
+    dotChanger();
   } else {
     cur_slide--;
     Move_Slide(cur_slide);
+    dotChanger();
   }
 });
+
+const dots = document.querySelector(".F_Dots");
+console.log(dots);
+console.log("hello");
+
+dots.addEventListener("click", (e) => {
+  if (e.target.classList.contains("btn")) {
+    const { index } = e.target.dataset;
+    cur_slide = index;
+    Move_Slide(cur_slide);
+    dotChanger();
+  }
+});
+
+// function dotClicker(index) {
+//   cur_slide = index;
+//   Move_Slide(cur_slide);
+//   dotChanger();
+// }
+
+function dotChanger() {
+  let allbtns = document.querySelectorAll(".btn");
+  console.log(allbtns);
+
+  for (let i = 0; i <= allbtns.length - 1; i++) {
+    if (i == cur_slide) {
+      allbtns[i].style.background = "red";
+    } else {
+      allbtns[i].style.background = "gray";
+    }
+  }
+}
+
+dotChanger();
